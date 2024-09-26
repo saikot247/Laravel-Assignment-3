9f150f135199a1d14edd8905a07259ef0b77c095
Saikot Biswas <84744165+saikot247@users.noreply.github.com>
2024-09-26 18:43:52 +0600

    Add files via upload

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index(Request $request) {
+        $name = "Donal Trump";
+        $age = "75";
+        $data = [
+            "id" => $request->id,
+            "name" => $name,
+            "age" => $age
+        ];
+        $cookie_name = "access_token";
+        $value = "123-XYZ";
+        $minutes = "1";
+        $path = "/";
+        $domain = $_SERVER["SERVER_NAME"];
+        $secure = false;
+        $httpOnly = true;
+        $cookie = cookie($cookie_name, $value, $minutes, $path, $domain, $secure, $httpOnly);
+        return response()->json($data, 200)->cookie($cookie);
+    }
+}
